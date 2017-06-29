b6284b091f123d16ada9b38ac91bd3fa57f739b9
takahiro <sequence2014@gmail.com>
2017-06-29 18:53:53 +0900

    flask_tutorial(Japanese) ch.4.1

--- flaskr/views.py
+++ flaskr/views.py
@@ -19,3 +19,24 @@ def add_entry():
     db.session.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
+
+@app.route('/users/')
+def user_list():
+    return 'list users'
+
+@app.route('/users/<int:user_id>/')
+def user_detail(user_id):
+    return 'detail user ' + str(user_id)
+
+@app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
+def user_edit(user_id):
+    return 'edit user ' + str(user_id)
+
+@app.route('/users/create/', methods=['GET', 'POST'])
+def user_create():
+    return 'create a new user'
+
+@app.route('/users/<int:user_id>/delete/', methods=['DELETE'])
+def user_delete(user_id):
+    return NotImplementedError('DELETE')
--- manage.py
+++ manage.py
@@ -1,2 +1,12 @@
-from flaskr import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+from __future__ import print_function
+from flask.ext.script import Manager
+from flaskr import app, db
+
+manager = Manager(app)
+
+@manager.command
+def init_db():
+    db.create_all()
+
+if __name__ == '__main__':
+    manager.run()
