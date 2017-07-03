0b70540295a9a6facbba8c2a4ce2f91d26e1bf81
takahiro <sequence2014@gmail.com>
2017-07-03 13:19:19 +0900

    update API for using Resourceful_Routing

--- flask_restful/api.py
+++ flask_restful/api.py
@@ -6,17 +6,23 @@ https://flask-restful.readthedocs.io/en/0.3.5/quickstart.html
 を参考にflaskのAPIを実装しています。
 """
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+todos = {}
 
-api.add_resource(HelloWorld, '/')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
